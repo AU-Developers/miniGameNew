6ec5ef43a98612206159cc17e0f8ab5aaf9059c5
gjandausdn <60877618+gjandausdn@users.noreply.github.com>
2020-02-19 14:29:44 +0800

    Updated GameController and PlayerData
    Created a function called RandomGenerationOfStoppingValue
    
    and a stoppingPoint variable in PlayerData

--- Assets/scripts/modified scripts/PlayerData.cs
+++ Assets/scripts/modified scripts/PlayerData.cs
@@ -7,7 +7,8 @@ namespace Minigame
     [CreateAssetMenu(menuName = "Player Data", fileName = "New Player Data.asset")]
     public class PlayerData : ScriptableObject
     {
-        [Range(0, 100)] public float stoppingPoint = 20;
+        [Range(0, 100)] public float winChanceRange = 20;
         [Range(1, 100)] public float goodChanceRange = 30, perfectChanceRange = 10;
+        [SerializeField] public float stoppingPoint;
     }
 }
